1c5b627f0de638ba3a35568851da9028fd29c9de
Alberto Marchetti <cmaster11@gmail.com>
2019-10-16 11:26:02 +0300

    added inverse regex support

--- regex.go
+++ regex.go
@@ -1,9 +1,35 @@
 package k8seventwatcher
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 type Regexp struct {
-	*regexp.Regexp
+	regex *regexp.Regexp
+
+	inverseMatch bool
+}
+
+func NewRegexp(pattern string) (*Regexp, error) {
+	r := &Regexp{}
+
+	// Is this an inverse match?
+	if strings.HasPrefix(pattern, "!") {
+		r.inverseMatch = true
+		pattern = pattern[1:]
+	} else {
+		r.inverseMatch = false
+	}
+
+	var err error
+
+	r.regex, err = regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
 
 func (r *Regexp) UnmarshalYAML(unmarshal func(interface{}) error) error {
@@ -13,14 +39,37 @@ func (r *Regexp) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return err
 	}
 
-	(*r).Regexp, err = regexp.Compile(regexString)
+	newRegex, err := NewRegexp(regexString)
 	if err != nil {
 		return err
 	}
+
+	r.inverseMatch = newRegex.inverseMatch
+	r.regex = newRegex.regex
 
 	return nil
 }
 
 func (r *Regexp) MarshalYAML() (interface{}, error) {
-	return r.Regexp.String(), nil
+	return r.String(), nil
+}
+
+func (r *Regexp) String() string {
+	regexString := r.regex.String()
+
+	if r.inverseMatch {
+		regexString = "!" + regexString
+	}
+
+	return regexString
+}
+
+func (r *Regexp) MatchString(value string) bool {
+	matches := r.regex.MatchString(value)
+
+	if r.inverseMatch {
+		matches = !matches
+	}
+
+	return matches
 }
---
+++ regex_test.go
@@ -0,0 +1,30 @@
+package k8seventwatcher
+
+import "testing"
+
+func TestRegexpInverse(t *testing.T) {
+	regexString := "hello"
+	regex, err := NewRegexp(regexString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !regex.MatchString("hello123") {
+		t.Fatal("no match")
+	}
+
+	regexInverseString := "!hello"
+	regexInverse, err := NewRegexp(regexInverseString)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if regexInverse.MatchString("hello123") {
+		t.Fatal("no match")
+	}
+
+	if !regexInverse.MatchString("mello123") {
+		t.Fatal("no match")
+	}
+
+}
